2985724cbc7cca62f8e818387ddae7b2cae277ac
Jakub Dudziński <jdudzinski101@gmail.com>
2021-03-28 15:48:03 +0200

    Add project files.

---
+++ Engine/WebConnector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Engine
+{
+    public class WebConnector
+    {
+        private string baseUrl;
+
+        //https://api.covid19api.com/total/country/poland?from=2021-03-26T01:00:00Z&to=2021-03-27T00:00:00Z
+
+        public WebConnector(string bUrl)
+        {
+            baseUrl = bUrl;
+        }
+    }
+}
---
+++ WebApiApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApiApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
