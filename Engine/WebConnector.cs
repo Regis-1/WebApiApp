3be2f74b334699a182a5838b3a8d5a780f5e3904
Jakub Dudziński <jdudzinski101@gmail.com>
2021-03-30 13:06:52 +0200

    Added JsonParser class and upgraded WebConnector methods

--- Engine/WebConnector.cs
+++ Engine/WebConnector.cs
@@ -1,16 +1,91 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web;
 
 namespace Engine
 {
     public class WebConnector
     {
         private string baseUrl;
-
-        //https://api.covid19api.com/total/country/poland?from=2021-03-26T01:00:00Z&to=2021-03-27T00:00:00Z
+        private string connectUrl = "";
 
         public WebConnector(string bUrl)
         {
             baseUrl = bUrl;
         }
+
+        /*
+         * Set connection path and parameters. Needed for changing path to connect or filter values.
+         * Function constructs "connectUrl" for later usage.
+         * 
+         * path -> path to specific file eg. /total/poland/
+         * parameters -> parameters for HTTPS GET method eg. ...?status=confirmed
+         */
+        private void SetConnection(string path, Dictionary<string, string> parameters)
+        {
+            connectUrl = baseUrl + path + "?";
+            foreach (KeyValuePair<string,string> kvp in parameters)
+            {
+                connectUrl += kvp.Key+"="+kvp.Value+"&";
+            }
+            connectUrl = connectUrl.Remove(connectUrl.Length - 1, 1);
+        }
+
+        private void SetConnection(string path)
+        {
+            connectUrl = baseUrl + path;
+        }
+
+        /*
+         * Sets the connector for getting data about the newest total data of given country
+         * 
+         * country -> written in english name of a country eg. "poland", "france" etc.
+         */
+        public void SetRecentTotalByCountry(string country)
+        {
+            DateTime yesterday = DateTime.Now.AddDays(-1);
+            DateTime dayBefore = yesterday.AddDays(-1);
+            string fYesterday = FormatDate(yesterday);
+            string fDayBefore = FormatDate(dayBefore);
+
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("from", fDayBefore);
+            parameters.Add("to", fYesterday);
+
+            SetConnection("total/country/"+country, parameters);
+        }
+
+        public void SetGlobalSummary()
+        {
+            SetConnection("summary");
+        }
+
+        /*
+         * Connect to set url. Function returns response string.
+         * Response string can be used for JSON deserialization.
+         */
+        public string Connect()
+        {
+            var response = ConnectAsync();
+            string r = response.Result.ToString();
+
+            return r;
+        }
+
+        private async System.Threading.Tasks.Task<string> ConnectAsync()
+        {
+            Console.WriteLine("Connecting to " + connectUrl);
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage response = await client.GetAsync(connectUrl);
+            string r = await response.Content.ReadAsStringAsync();
+            return r;
+        }
+
+        /*
+         * Function used only for returning formatted data accepted by API
+         */
+        private string FormatDate(DateTime d) => d.ToString("yyyy-MM-dd") + "T01:00:00Z";
     }
 }
--- WebApiApp/Program.cs
+++ WebApiApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Engine;
 
 namespace WebApiApp
 {
@@ -6,7 +9,13 @@ namespace WebApiApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WebConnector wc = new WebConnector("https://api.covid19api.com/");
+
+            wc.SetRecentTotalByCountry("poland");
+            foreach (CountryData cd in JsonParser.ExtractListData<CountryData>(wc.Connect()))
+            {
+                Console.WriteLine(cd.Country);
+            }
         }
     }
 }
